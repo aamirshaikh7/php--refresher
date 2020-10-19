eb0c4e4b1776973069b2bdf3c5b80694bfd3ad97
Aamir shaikh <aamirshaikh3232@gmail.com>
2020-10-19 16:56:20 +0530

    creating add employee form

--- PDO-CRUD/add.view.php
+++ PDO-CRUD/add.view.php
@@ -3,9 +3,30 @@
     <div class="container" id="get-started">
         <div class="section">
 
-        <form action="">
-            
-        </form>
+        <div class="row">
+            <form class="col s12" method="POST">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input name="name" id="name" type="text" class="validate">
+                        <label for="name">Name</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">work</i>
+                        <input name="designation" id="designation" type="text" class="validate">
+                        <label for="designation">Designation</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">mail</i>
+                        <input name="email" id="email" type="email" class="validate">
+                        <label for="mail">E-mail</label>
+                    </div>
+                    <div class="col s12">
+                    <a type="submit" class="btn-large center waves-effect waves-light blue">Submit</a>
+                    </div>
+                </div>
+            </form>
+        </div>
                 
         </div>
     
